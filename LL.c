7980f94508457206ac9fba059f92a40f6a1003b6
banke58 <55846128+banke58@users.noreply.github.com>
2022-10-08 19:31:17 +0530

    update LL.cpp
    update code from c to c++

--- LL.c
+++ LL.c
@@ -1,248 +1,175 @@
-#include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
+// C++ program for the above approach
+#include <iostream>
+using namespace std;
 
-struct node {
-   int data;
-   int key;
-   struct node *next;
+// Node class to represent
+// a node of the linked list.
+class Node {
+public:
+	int data;
+	Node* next;
+
+	// Default constructor
+	Node()
+	{
+		data = 0;
+		next = NULL;
+	}
+
+	// Parameterised Constructor
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
 };
 
-struct node *head = NULL;
-struct node *current = NULL;
+// Linked list class to
+// implement a linked list.
+class Linkedlist {
+	Node* head;
 
-//display the list
-void printList() {
-   struct node *ptr = head;
-   printf("\n[ ");
-	
-   //start from the beginning
-   while(ptr != NULL) {
-      printf("(%d,%d) ",ptr->key,ptr->data);
-      ptr = ptr->next;
-   }
-	
-   printf(" ]");
+public:
+	// Default constructor
+	Linkedlist() { head = NULL; }
+
+	// Function to insert a
+	// node at the end of the
+	// linked list.
+	void insertNode(int);
+
+	// Function to print the
+	// linked list.
+	void printList();
+
+	// Function to delete the
+	// node at given position
+	void deleteNode(int);
+};
+
+// Function to delete the
+// node at given position
+void Linkedlist::deleteNode(int nodeOffset)
+{
+	Node *temp1 = head, *temp2 = NULL;
+	int ListLen = 0;
+
+	if (head == NULL) {
+		cout << "List empty." << endl;
+		return;
+	}
+
+	// Find length of the linked-list.
+	while (temp1 != NULL) {
+		temp1 = temp1->next;
+		ListLen++;
+	}
+
+	// Check if the position to be
+	// deleted is less than the length
+	// of the linked list.
+	if (ListLen < nodeOffset) {
+		cout << "Index out of range"
+			<< endl;
+		return;
+	}
+
+	// Declare temp1
+	temp1 = head;
+
+	// Deleting the head.
+	if (nodeOffset == 1) {
+
+		// Update head
+		head = head->next;
+		delete temp1;
+		return;
+	}
+
+	// Traverse the list to
+	// find the node to be deleted.
+	while (nodeOffset-- > 1) {
+
+		// Update temp2
+		temp2 = temp1;
+
+		// Update temp1
+		temp1 = temp1->next;
+	}
+
+	// Change the next pointer
+	// of the previous node.
+	temp2->next = temp1->next;
+
+	// Delete the node
+	delete temp1;
 }
 
-//insert link at the first location
-void insertFirst(int key, int data) {
-   //create a link
-   struct node *link = (struct node*) malloc(sizeof(struct node));
-	
-   link->key = key;
-   link->data = data;
-	
-   //point it to old first node
-   link->next = head;
-	
-   //point first to new first node
-   head = link;
+// Function to insert a new node.
+void Linkedlist::insertNode(int data)
+{
+	// Create the new Node.
+	Node* newNode = new Node(data);
+
+	// Assign to head
+	if (head == NULL) {
+		head = newNode;
+		return;
+	}
+
+	// Traverse till end of list
+	Node* temp = head;
+	while (temp->next != NULL) {
+
+		// Update temp
+		temp = temp->next;
+	}
+
+	// Insert at the last.
+	temp->next = newNode;
 }
 
-//delete first item
-struct node* deleteFirst() {
+// Function to print the
+// nodes of the linked list.
+void Linkedlist::printList()
+{
+	Node* temp = head;
 
-   //save reference to first link
-   struct node *tempLink = head;
-	
-   //mark next to first link as first 
-   head = head->next;
-	
-   //return the deleted link
-   return tempLink;
+	// Check for empty list.
+	if (head == NULL) {
+		cout << "List empty" << endl;
+		return;
+	}
+
+	// Traverse the list.
+	while (temp != NULL) {
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
 }
 
-//is list empty
-bool isEmpty() {
-   return head == NULL;
-}
+// Driver Code
+int main()
+{
+	Linkedlist list;
 
-int length() {
-   int length = 0;
-   struct node *current;
-	
-   for(current = head; current != NULL; current = current->next) {
-      length++;
-   }
-	
-   return length;
-}
+	// Inserting nodes
+	list.insertNode(1);
+	list.insertNode(2);
+	list.insertNode(3);
+	list.insertNode(4);
 
-//find a link with given key
-struct node* find(int key) {
+	cout << "Elements of the list are: ";
 
-   //start from the first link
-   struct node* current = head;
+	// Print the list
+	list.printList();
+	cout << endl;
 
-   //if list is empty
-   if(head == NULL) {
-      return NULL;
-   }
+	// Delete node at position 2.
+	list.deleteNode(2);
 
-   //navigate through list
-   while(current->key != key) {
-	
-      //if it is last node
-      if(current->next == NULL) {
-         return NULL;
-      } else {
-         //go to next link
-         current = current->next;
-      }
-   }      
-	
-   //if data found, return the current Link
-   return current;
-}
-
-//delete a link with given key
-struct node* delete(int key) {
-
-   //start from the first link
-   struct node* current = head;
-   struct node* previous = NULL;
-	
-   //if list is empty
-   if(head == NULL) {
-      return NULL;
-   }
-
-   //navigate through list
-   while(current->key != key) {
-
-      //if it is last node
-      if(current->next == NULL) {
-         return NULL;
-      } else {
-         //store reference to current link
-         previous = current;
-         //move to next link
-         current = current->next;
-      }
-   }
-
-   //found a match, update the link
-   if(current == head) {
-      //change first to point to next link
-      head = head->next;
-   } else {
-      //bypass the current link
-      previous->next = current->next;
-   }    
-	
-   return current;
-}
-
-void sort() {
-
-   int i, j, k, tempKey, tempData;
-   struct node *current;
-   struct node *next;
-	
-   int size = length();
-   k = size ;
-	
-   for ( i = 0 ; i < size - 1 ; i++, k-- ) {
-      current = head;
-      next = head->next;
-		
-      for ( j = 1 ; j < k ; j++ ) {   
-
-         if ( current->data > next->data ) {
-            tempData = current->data;
-            current->data = next->data;
-            next->data = tempData;
-
-            tempKey = current->key;
-            current->key = next->key;
-            next->key = tempKey;
-         }
-			
-         current = current->next;
-         next = next->next;
-      }
-   }   
-}
-
-void reverse(struct node** head_ref) {
-   struct node* prev   = NULL;
-   struct node* current = *head_ref;
-   struct node* next;
-	
-   while (current != NULL) {
-      next  = current->next;
-      current->next = prev;   
-      prev = current;
-      current = next;
-   }
-	
-   *head_ref = prev;
-}
-
-void main() {
-   insertFirst(1,10);
-   insertFirst(2,20);
-   insertFirst(3,30);
-   insertFirst(4,1);
-   insertFirst(5,40);
-   insertFirst(6,56); 
-
-   printf("Original List: "); 
-	
-   //print list
-   printList();
-
-   while(!isEmpty()) {            
-      struct node *temp = deleteFirst();
-      printf("\nDeleted value:");
-      printf("(%d,%d) ",temp->key,temp->data);
-   }  
-	
-   printf("\nList after deleting all items: ");
-   printList();
-   insertFirst(1,10);
-   insertFirst(2,20);
-   insertFirst(3,30);
-   insertFirst(4,1);
-   insertFirst(5,40);
-   insertFirst(6,56);
-   
-   printf("\nRestored List: ");
-   printList();
-   printf("\n");  
-
-   struct node *foundLink = find(4);
-	
-   if(foundLink != NULL) {
-      printf("Element found: ");
-      printf("(%d,%d) ",foundLink->key,foundLink->data);
-      printf("\n");  
-   } else {
-      printf("Element not found.");
-   }
-
-   delete(4);
-   printf("List after deleting an item: ");
-   printList();
-   printf("\n");
-   foundLink = find(4);
-	
-   if(foundLink != NULL) {
-      printf("Element found: ");
-      printf("(%d,%d) ",foundLink->key,foundLink->data);
-      printf("\n");
-   } else {
-      printf("Element not found.");
-   }
-	
-   printf("\n");
-   sort();
-	
-   printf("List after sorting the data: ");
-   printList();
-	
-   reverse(&head);
-   printf("\nList after reversing the data: ");
-   printList();
+	cout << "Elements of the list are: ";
+	list.printList();
+	cout << endl;
+	return 0;
 }
